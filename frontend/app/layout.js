d84308ed7b733e659b05fa53f9af729f125c3c4e
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-12 02:42:27 +0200

    Load page-specific UI components

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -20,6 +20,7 @@ import "./navigation-cleanup.css";
 import "./design-system.css";
 import "./accessibility.css";
 import "./ui-coherence.css";
+import "./ui-components.css";
 import { Cairo } from "next/font/google";
 import AppBootstrap from "./AppBootstrap";
 import PlatformClient from "./PlatformClient";
