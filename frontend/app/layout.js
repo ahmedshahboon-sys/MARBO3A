25f55423e4a42cd406a63f2dcb49c3ec10654009
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-12 16:13:19 +0200

    Load final V3 audit contract last

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -36,6 +36,7 @@ import "./ui-v3-density.css";
 import "./ui-v3-controls.css";
 import "./ui-v3-reference.css";
 import "./ui-v3-features.css";
+import "./ui-v3-final-audit.css";
 import { Cairo } from "next/font/google";
 import AppBootstrap from "./AppBootstrap";
 import PlatformClient from "./PlatformClient";
