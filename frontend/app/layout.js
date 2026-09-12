a527c770512e2e78150d0e0936b915f45b019aec
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-12 10:18:53 +0200

    Load V3 messages styling

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -25,6 +25,7 @@ import "./ui-v3.css";
 import "./ui-v3-screens.css";
 import "./ui-v3-settings.css";
 import "./ui-v3-rooms.css";
+import "./ui-v3-messages.css";
 import "./ui-v3-brand.css";
 import { Cairo } from "next/font/google";
 import AppBootstrap from "./AppBootstrap";
