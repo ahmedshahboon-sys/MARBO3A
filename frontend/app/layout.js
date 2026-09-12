fff07e9a3db32a80008f32543b71c3e2cff34cff
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-12 17:25:57 +0200

    Load canonical navigation styling after production QA

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -38,6 +38,7 @@ import "./ui-v3-reference.css";
 import "./ui-v3-features.css";
 import "./ui-v3-final-audit.css";
 import "./ui-v3-production-qa.css";
+import "./ui-v3-navigation.css";
 import { Cairo } from "next/font/google";
 import AppBootstrap from "./AppBootstrap";
 import PlatformClient from "./PlatformClient";
