90292910c4ada30f5a9d0331d10c0d98d56592fa
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-13 15:36:39 +0200

    Link advanced admin from main admin center

--- frontend/app/admin/page.js
+++ frontend/app/admin/page.js
@@ -1,2 +1,4 @@
+import Link from "next/link";
 import AdminCenter from "./AdminCenter";
-export default function AdminPage(){return <AdminCenter/>}
+import styles from "./AdminPage.module.css";
+export default function AdminPage(){return <><div className={styles.advancedEntry}><Link href="/admin/advanced">فتح Group O · الإدارة المتقدمة</Link></div><AdminCenter/></>}
