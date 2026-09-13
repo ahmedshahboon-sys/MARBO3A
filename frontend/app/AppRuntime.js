46d0cc387ff9e4b3b70aca55f78a4fd2a5583851
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-13 03:29:24 +0200

    Keep route-aware runtime with stable static imports

--- frontend/app/AppRuntime.js
+++ frontend/app/AppRuntime.js
@@ -1,26 +1,24 @@
 "use client";
-import dynamic from "next/dynamic";
 import {usePathname} from "next/navigation";
 import {isAppShellPath} from "./navigation-policy";
-
-const AppBootstrap=dynamic(()=>import("./AppBootstrap"),{ssr:false});
-const PlatformClient=dynamic(()=>import("./PlatformClient"),{ssr:false});
-const NavigationRuntime=dynamic(()=>import("./NavigationRuntime"),{ssr:false});
-const SessionGuard=dynamic(()=>import("./SessionGuard"),{ssr:false});
-const RealtimeClient=dynamic(()=>import("./RealtimeClient"),{ssr:false});
-const AppDataCoordinator=dynamic(()=>import("./AppDataCoordinator"),{ssr:false});
-const TypingRuntime=dynamic(()=>import("./TypingRuntime"),{ssr:false});
-const SocialHomeRedirect=dynamic(()=>import("./SocialHomeRedirect"),{ssr:false});
-const DebugTrace=dynamic(()=>import("./DebugTrace"),{ssr:false});
-const CallCenter=dynamic(()=>import("./CallCenter"),{ssr:false});
-const OnboardingGate=dynamic(()=>import("./OnboardingGate"),{ssr:false});
-const ContextSafety=dynamic(()=>import("./ContextSafety"),{ssr:false});
-const ReactionHoldBridge=dynamic(()=>import("./ReactionHoldBridge"),{ssr:false});
-const PremiumChrome=dynamic(()=>import("./PremiumChrome"),{ssr:false});
-const SettingsPanel=dynamic(()=>import("./SettingsPanel"),{ssr:false});
-const GuestBrowseEntry=dynamic(()=>import("./GuestBrowseEntry"),{ssr:false});
-const SideDrawer=dynamic(()=>import("./SideDrawer"),{ssr:false});
-const SocialDock=dynamic(()=>import("./SocialDock"),{ssr:false});
+import AppBootstrap from "./AppBootstrap";
+import PlatformClient from "./PlatformClient";
+import NavigationRuntime from "./NavigationRuntime";
+import SessionGuard from "./SessionGuard";
+import RealtimeClient from "./RealtimeClient";
+import AppDataCoordinator from "./AppDataCoordinator";
+import TypingRuntime from "./TypingRuntime";
+import SocialHomeRedirect from "./SocialHomeRedirect";
+import DebugTrace from "./DebugTrace";
+import CallCenter from "./CallCenter";
+import OnboardingGate from "./OnboardingGate";
+import ContextSafety from "./ContextSafety";
+import ReactionHoldBridge from "./ReactionHoldBridge";
+import PremiumChrome from "./PremiumChrome";
+import SettingsPanel from "./SettingsPanel";
+import GuestBrowseEntry from "./GuestBrowseEntry";
+import SideDrawer from "./SideDrawer";
+import SocialDock from "./SocialDock";
 
 export default function AppRuntime({children}){
   const path=usePathname(),app=isAppShellPath(path);
