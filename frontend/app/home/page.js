3e390b29fdcf2b274a8c239e6b6e3387e3854419
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-13 19:21:55 +0200

    Surface marketplace preview on home

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -1,5 +1,6 @@
 "use client";
 import HomeStories from "../HomeStories";
 import PublicRoomRail from "../PublicRoomRail";
+import MarketplaceComingSoon from "../MarketplaceComingSoon";
 import SocialFeed from "../SocialFeed";
-export default function HomePage(){return <><HomeStories/><PublicRoomRail/><SocialFeed home/></>}
+export default function HomePage(){return <><HomeStories/><PublicRoomRail/><MarketplaceComingSoon compact/><SocialFeed home/></>}
