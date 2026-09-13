cca2a87d38e3d63fd5eba1001b2eb55839822fd7
ahmedshahboon-sys <ahmed.shahboon@gmail.com>
2026-09-13 20:02:08 +0200

    Load canonical brand override after legacy visual layers

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -50,6 +50,7 @@ import "./first-run-stability.css";
 import "./ui-v3-social-experience.css";
 import "./r1-core.css";
 import "./r1-admin.css";
+import "./r1-brand-override.css";
 import "./ui-v3-unified-scale.css";
 import { Cairo } from "next/font/google";
 import AppRuntime from "./AppRuntime";
